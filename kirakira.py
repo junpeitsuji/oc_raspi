87086fc49ef0d9ba7cb82ebb0f5764603f63bb7b
Junpei Tsuji <tsujimotter@gmail.com>
2026-05-21 15:25:56 +0900

    コメントを追加

--- kirakira.py
+++ kirakira.py
@@ -1,16 +1,19 @@
-from gpiozero import RGBLED
-from gpiozero import Button
+from gpiozero import RGBLED, Button
 from time import sleep
 
-led = RGBLED(red=9, green=10, blue=11)
-
+# タクトスイッチをGPIO 2に接続
 button = Button(2)
 
+# フルカラーLEDをGPIO 9〜11に接続
+led = RGBLED(red=9, green=10, blue=11)
+
+# 順に表示するカラーを配列に設定（赤、黄、緑、シアン、青、紫）
 colors = [(1,0,0), (1,1,0), (0,1,0), (0,1,1), (0,0,1), (1,0,1)]
 
 i = 0
 while True:
     if button.is_pressed:
+        # スイッチが押されたときだけ色を変更
         led.color = colors[i % 6]
         sleep(0.05)
         i += 1
--- led.py
+++ led.py
@@ -1,9 +1,10 @@
 from gpiozero import RGBLED
 
+# フルカラーLEDをGPIO 9〜11に接続
 led = RGBLED(red=9, green=10, blue=11)
 
 led.red = 0.0
 led.green = 0.0
 led.blue = 1.0
 
-input()
+input()   # キーボード入力待ち
